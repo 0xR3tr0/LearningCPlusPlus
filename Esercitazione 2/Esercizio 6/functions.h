3bb469d43e3d8b0bb22300e22230a4d6bdbe99df
Cikuozzo <roccorotondo2003@protonmail.com>
2022-12-16 16:07:34 +0100

    16/12/2022

---
+++ Esercitazione 2/Esercizio 6/functions.h
@@ -0,0 +1,3 @@
+void input_cooordinate(float myArrayX[], float myArrayY[], int len);
+void stampa_array(float myArrayX[], float myArrayY[], int len);
+void distanza(float myArrayX[], float myArrayY[], int len, float x0, float y0);
---
+++ Esercitazione 2/Esercizio 6/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "functions.h"
+
+using namespace std;
+
+int main(){
+
+  float x0, y0; 
+  int len;
+
+  cout << "Inserisci la coordinata x0: ";
+  cin >> x0;
+
+  cout << "Inserisci la coordinata y0: ";
+  cin >> y0;
+
+  cout << "Inserisci il numero di punti: ";
+  cin >> len;
+
+  float* myArrayX = new float[len];
+  float* myArrayY = new float[len];
+
+  input_cooordinate(myArrayX, myArrayY, len);
+
+  stampa_array(myArrayX, myArrayY, len);
+
+  distanza(myArrayX, myArrayY, len, x0, y0);
+
+  }
+
+  
+
