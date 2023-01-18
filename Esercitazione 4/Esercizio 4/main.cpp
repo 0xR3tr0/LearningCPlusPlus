11898d4df8e3e920cb36d799cd4dac5b31232642
Cikuozzo <roccorotondo2003@protonmail.com>
2023-01-18 14:15:32 +0100

    WIP

--- Esercitazione 4/Esercizio 4/main.cpp
+++ Esercitazione 4/Esercizio 4/main.cpp
@@ -48,11 +48,15 @@ void inserisciCorso(string *corso){
 
     }
 
+    return;
+
 }
 
 Studente inserisciStudente(){
 
     Studente tmp;
+
+    cin.ignore();
 
     cout << "Inserisci il nome dello studente: ";
     getline(cin, tmp.nome);
@@ -66,40 +70,85 @@ Studente inserisciStudente(){
     cin >> tmp.media;
 
     cout << "Inserisci l'anno di corso: ";
-    cin >> tmp.corso;
+    cin >> tmp.anno;
+
+    return tmp;
 
 }
 
-void numStudenti(int array[], int *n){
+void numStudenti(Studente array[], int *n){
 
     do{
 
     cout << "Quanti studenti vuoi inizializzare?: ";
-    cin >> n;
+    cin >> *n;
 
-    if(n <= 0 || n > 100){
+    if(*n <= 0 || *n > 100){
 
         cout << "Numero non valido(Inserire un numero compreso tra 1 e 100)";
 
     }
 
-    }while(n <= 0 || n > 100);
+    }while(*n <= 0 || *n > 100);
 
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < *n; i++){
 
         array[i] = inserisciStudente();
-
+        
+        cout << endl;
+        cout << "----------------------------------------------" << endl;
+        cout << endl;
     }
+
 
     return;
 
 }
 
+void chiediCorso(string *corso){
 
+    int corsoTmp;
+
+    do{
+
+        cout << "Quale corso si vuole visualizzare?" << endl;
+        cout << "1 - Informatica" << endl;
+        cout << "2 - Industriale" << endl;
+        cout << "3 - Civile" << endl;
+
+        cin >> corsoTmp;
+
+    }while(corsoTmp < 1 || corsoTmp > 3);
+
+    switch (corsoTmp){
+
+        case 1:
+
+            *corso = "INF";
+            break;
+            
+        case 2:
+
+            *corso = "IND";
+            break;
+
+        case 3:
+
+            *corso = "CIV";
+            break;
+
+        }
+
+    return;
+
+}
 
 int main(){
 
+    int nStud = 0;
+
     Studente arrayStud[NMAX];
 
+    numStudenti(arrayStud, &nStud);
 
 }
